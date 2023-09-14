565beb452d11302b4e6196a963ba44e4ab6a96e8
Alex Johnson <johnsonav1992@gmail.com>
2023-09-14 05:50:36 -0500

    fix main imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+
+// MUI
 import { CssBaseline } from '@mui/joy';
+
+// Components
+import App from './App.tsx';
 
 ReactDOM.createRoot( document.getElementById( 'root' )! ).render(
     <React.StrictMode>
