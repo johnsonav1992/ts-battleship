2e1ee541130d042c8b06022a196b2b12ba8cfd9c
Alex Johnson <johnsonav1992@gmail.com>
2023-09-18 06:01:16 -0500

    flash animation for now

--- src/Components/ShotMarker/ShotMarker.tsx
+++ src/Components/ShotMarker/ShotMarker.tsx
@@ -1,11 +1,20 @@
 // MUI
-import { Box } from '@mui/joy';
+import {
+    Box
+    , useTheme
+} from '@mui/joy';
 
 interface Props {
     type: 'hit' | 'miss';
 }
 
 const ShotMarker = ( { type }: Props ) => {
+    const theme = useTheme();
+
+    const pegColor = type === 'hit'
+        ? theme.palette.danger[ 500 ]
+        : theme.palette.common.white;
+
     return (
         <Box
             sx={ {
@@ -13,9 +22,28 @@ const ShotMarker = ( { type }: Props ) => {
                 , position: 'absolute'
                 , width: '25%'
                 , height: '25%'
-                , backgroundColor: theme => type === 'hit'
-                    ? theme.palette.danger[ 500 ]
-                    : 'common.white'
+                , backgroundColor: pegColor
+                , '&::before': {
+                    content: '""'
+                    , position: 'absolute'
+                    , animation: 'flash .2s ease-out forwards'
+                    , border: `.1rem solid ${ pegColor }`
+                    , borderRadius: '100%'
+                    , width: '2rem'
+                    , height: '2rem'
+                    , top: '-80%'
+                    , left: '-80%'
+                }
+                , '@keyframes flash': {
+                    '0%': {
+                        opacity: 1
+                        , transform: 'scale(1)'
+                    }
+                    , '100%': {
+                        opacity: 0
+                        , transform: 'scale(2)'
+                    }
+                }
             } }
         />
     );
